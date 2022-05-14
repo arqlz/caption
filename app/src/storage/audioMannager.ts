38a34b5f1db3ecb5ccb07adf2f7203643a8a579b
Jesus Zorrilla <solidosconstructora@gmail.com>
2022-05-14 08:43:13 -0400

    0

--- app/src/storage/audioMannager.ts
+++ app/src/storage/audioMannager.ts
@@ -17,12 +17,23 @@ loadCredentials(credentials => {
     blobServiceClient.getContainerClient("transcripcion").exists().then(existe => {
       if (!existe) blobServiceClient.createContainer("transcripcion");
     })
+
+    blobServiceClient.getContainerClient("rawtranscripcion").exists().then(existe => {
+      if (!existe) blobServiceClient.createContainer("rawtranscripcion");
+    })
+
+    blobServiceClient.getContainerClient("images").exists().then(existe => {
+      if (!existe) {
+        blobServiceClient.createContainer("images");
+        blobServiceClient.getContainerClient("images").setAccessPolicy("blob")
+      }
+      else {
+        blobServiceClient.getContainerClient("images").setAccessPolicy("blob")
+      }
+    })
     console.log("Storage ready.")      
     
 })
-
-
-
 
 
 export function crearSesionAlmacenamiento(uid: string)  {
@@ -39,12 +50,34 @@ export function crearSesionAlmacenamiento(uid: string)  {
 }
 
 export function crearSesionTranscripcion(uid: string)  {
-  const containerClient = blobServiceClient.getContainerClient("transcripcion");
+  const containerClient = blobServiceClient.getContainerClient("rawtranscripcion");
   var stream = new Readable({read: () => null}); 
-  if (process.env.PORT) containerClient.getBlockBlobClient(uid).uploadStream(stream); 
+  if (process.env.PORT) {
+    containerClient.getBlockBlobClient(uid).uploadStream(stream); 
+  }
   else {
     var writeStream = fs.createWriteStream(__dirname+`/../../../public/data/${uid}.jsonl`, {encoding: "utf-8"})
     stream.pipe(writeStream);
   }
   return stream
+}
+
+export function saveTrascriptionFile(key: string, data: string) {
+  const containerClient = blobServiceClient.getContainerClient("transcripcion"); 
+  return containerClient.getBlockBlobClient(key).uploadData(Buffer.from(data))
+
+}
+
+export function saveFile(collection: string, key: string, data: Buffer) {
+  const containerClient = blobServiceClient.getContainerClient(collection);
+
+  return containerClient.getBlockBlobClient(key).uploadData(data).then(r => {
+    return containerClient.url+"/"+key;
+  })
+}
+
+
+export function getTrascriptionFile(key: string) {
+  const containerClient = blobServiceClient.getContainerClient("transcripcion");
+  return containerClient.getBlockBlobClient(key).downloadToBuffer()
 }
--- public/src/presenter.ts
+++ public/src/presenter.ts
@@ -1,20 +1,41 @@
 import { createDiv, createElement } from "./components/utils"
+declare const roomId: string;
 
 export class Presenter {
-    textContainner: HTMLDivElement;
+    transmissionContainner: HTMLDivElement;
     control: HTMLDivElement;
-    lines: {[id: string]: HTMLDivElement} = {}
+    mensajes: {[id: string]: HTMLDivElement} = {}
     queue: {result: string, id: string}[] = []
     raw: string = ""
-    constructor(rec?) {
+    stoped: boolean = false;
+    listennerMode: boolean = true;
+
+    private __title = "";
+    constructor( rec?) {
         var div = createDiv({width: "100%", height: "100%"})
         div.className="msgContainner";
+        div.id = "core"
 
-        this.control = createDiv()
+        let title = createDiv({margin: "15px 0"})
+        title.innerHTML = `Id de la sala: ${roomId}`;
+        div.append(title)
+
+        if (rec) this.listennerMode = false;
+
+        this.control = createDiv("navigation")
         var stopButton = createElement("button")
         stopButton.innerHTML = "STOP"
         stopButton.onclick = () => {
-            stopButton.disabled = true
+            if (this.stoped && this.listennerMode) {
+                stopButton.classList.remove("disabled")
+                stopButton.innerHTML = "STOP"
+                this.stoped = false;
+            } else {
+                stopButton.classList.add("disabled")
+                stopButton.innerHTML = "RESTART"
+                this.stoped = true;
+            }
+           
             if(rec) {
                 console.log("STOP RECORDER", rec)
                 rec.stop()
@@ -23,25 +44,45 @@ export class Presenter {
         this.control.append(stopButton)
         div.append(this.control)
 
-        this.textContainner = createDiv({width: 350,  margin: "auto auto", color: "#ffffff", fontSize: 20, 
-        display: "flex", flexDirection: "column", flexFlow: "column-reverse", overflow: "hidden", height: 120})
-        div.append(this.textContainner)
+        this.transmissionContainner = createDiv({width: 400, maxHeight: 200,  margin: "auto auto", color: "#ffffff", fontSize: 20, 
+        display: "flex", flexDirection: "column", overflow: "hidden"})
+        this.transmissionContainner.style.scrollBehavior = "smooth";
+        div.append(this.transmissionContainner)
 
         document.body.append(div)
     }
+
+    set title(value: string) {
+        this.__title = value;
+        
+    }
+    private render() {
+        if (this.listennerMode == true && this.stoped) return;
+        while(this.queue.length) {
+            var mensaje = this.queue.splice(0, 1)[0];
+            if (!this.mensajes[mensaje.id]) {
+                var div = createDiv("textRow",{ marginRight: "4pt"});
+                this.transmissionContainner.append(div);
+                this.mensajes[mensaje.id] = div;
+                this.mensajes[mensaje.id].innerHTML = mensaje.result;
+                this.transmissionContainner.scrollTo({top: this.transmissionContainner.scrollHeight})
+            } else {
+                this.mensajes[mensaje.id].innerHTML = mensaje.result;
+                this.transmissionContainner.scrollTo({top: this.transmissionContainner.scrollHeight})
+            }
+        }
+     
+    }
     append(data: {result: string, id: string}) {
         this.queue.push(data)   
-
-        if (!this.lines[data.id]) {
-            var div = createDiv("textRow",{ marginRight: "4pt"});
-            //this.textContainner.append(div);
-            this.textContainner.prepend(div);
-            this.lines[data.id] = div;
-            this.lines[data.id].innerHTML = data.result;
-        } else {
-            this.lines[data.id].innerHTML = data.result;
-        }
-      
+        this.render();  
+    }
+    stop() {
+        this.stoped = true;
+    }
+    resume() {
+        this.stoped = false;
+        this.render();
     }
 }
 export class Presenter2 {
